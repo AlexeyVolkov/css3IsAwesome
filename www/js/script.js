86580f319705ab1b8f1ad6425338a28cfe1fc4a3
Alexey <alexey.a.volkov@yandex.ru>
2020-01-23 15:55:10 +0400

    remove legacy

--- www/js/script.js
+++
@@ -1,9 +0,0 @@
-var audio = document.getElementById("myAudio");
-
-var bike = document.getElementById("bike");
-bike.onmouseover=function(){
-	audio.play();
-};
-bike.onmouseout=function(){
-	audio.pause();
-};
